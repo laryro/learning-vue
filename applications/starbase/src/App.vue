d48b933676d1c01754151179e3415885874683fc
Laryssa Rocha <laryssa.vrocha@gmail.com>
2019-03-22 15:28:52 -0300

    create app using vue-cli and fetches SWAPI to show a list of characters

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <div class="row">
+      <Character
+        :id="id"
+        v-for="(id, index) in initial_ids"
+        :key="index"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Character from './components/Character.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      'title': 'Generate Your Team',
+      initial_ids: [1, 13, 14]
+    }
+  },
+  components: {
+    Character
+  },
+}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ applications/starbase/src/components/Character.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="component character" @click="switchCharacter">
+    {{character}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      character: {}
+    }
+  },
+  methods: {
+    fetchCharacter(id) {
+      fetch(`https://swapi.co/api/people/${id}`, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => this.character = json)
+    },
+    switchCharacter() {
+      const randomId = Math.floor(Math.random() * 83) + 1;
+      this.fetchCharacter(randomId)
+    }
+  },
+  created() {
+    this.fetchCharacter(this.id)
+  },
+}
+</script>
